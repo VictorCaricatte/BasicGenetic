3e12d36637d2cd4a3710bfa782e50a153e92e367
Victor Caricatte <victorleniwys@gmail.com>
2025-08-12 10:07:26 -0300

    VariantGenV.py

--- VariantGenV/src/VariantGenV.py
+++ VariantGenV/src/VariantGenV.py
@@ -1,7 +1,7 @@
 """
 # ==============================================================================
 # Author:       Victor S Caricatte De Araújo
-# Email:        victorleniwys@gmail.com or victorsc@ufmg.br
+# Email:        victorsc@ufmg.br
 # Intitution:   Universidade federal de Minas Gerais
 # Version:      1.3.9
 # Date:         Jun, 4
